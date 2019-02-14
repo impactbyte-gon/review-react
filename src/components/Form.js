023bf88b039d4e0c381f8768fb1e72d16ef97f4a
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-14 12:00:59 +0700

    Put function to update state in component (setState)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 class Form extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      inputText: ''
+    }
+  }
+
+  handleChange = event => {
+    // this.handleChange = this.handleChange.bind(this)
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Your task here" />
+        <input
+          type="text"
+          placeholder="Your task here"
+          value={this.state.inputText}
+          onChange={this.handleChange}
+        />
         <input type="submit" value="Add" />
       </form>
     )
